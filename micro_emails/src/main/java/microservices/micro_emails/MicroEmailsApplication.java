57e97078e4cc642c2e3a5d700830a0efc0d04eeb
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-03-23 16:00:05 -0400

    FEAT: add micro_emails

---
+++ micro_emails/src/main/java/microservices/micro_emails/MicroEmailsApplication.java
@@ -0,0 +1,13 @@
+package microservices.micro_emails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEmailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEmailsApplication.class, args);
+	}
+
+}
