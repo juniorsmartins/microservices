48c7ebfdcd3d00092c845ba9101e7e766a6e1c66
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-04-09 16:09:39 -0400

    FEAT: add Bean Validation

--- micro_customers/src/main/java/microservices/micro_customers/adapter/dto/EnderecoDto.java
+++ micro_customers/src/main/java/microservices/micro_customers/adapter/dto/EnderecoDto.java
@@ -10,22 +10,22 @@ public record EnderecoDto(
     @Size(min = Constantes.MIN_CARACTERES_CUSTOMER_ENDERECO_CEP, max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_CEP)
     String cep,
 
-//    @Size(min = Constantes.MIN_CARACTERES_CUSTOMER_ENDERECO_ESTADO, max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_ESTADO)
+    @Size(min = Constantes.MIN_CARACTERES_CUSTOMER_ENDERECO_ESTADO, max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_ESTADO)
     String estado,
 
-//    @Size(max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_CIDADE)
+    @Size(max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_CIDADE)
     String cidade,
 
-//    @Size(max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_BAIRRO)
+    @Size(max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_BAIRRO)
     String bairro,
 
-//    @Size(max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_LOGRADOURO)
+    @Size(max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_LOGRADOURO)
     String logradouro,
 
-//    @Size(max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_NUMERO)
+    @Size(max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_NUMERO)
     String numero,
 
-//    @Size(max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_COMPLEMENTO)
+    @Size(max = Constantes.MAX_CARACTERES_CUSTOMER_ENDERECO_COMPLEMENTO)
     String complemento
 
 ) { }
