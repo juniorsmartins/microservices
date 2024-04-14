306bd3622e20077641c34512efa9962ddd32436f
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-04-14 14:52:31 -0400

    DOCS: anotações SpringDoc

--- micro_customers/src/main/java/microservices/micro_customers/adapter/dto/response/CustomerUpdateDtoResponse.java
+++ micro_customers/src/main/java/microservices/micro_customers/adapter/dto/response/CustomerUpdateDtoResponse.java
@@ -1,6 +1,7 @@
 package microservices.micro_customers.adapter.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import microservices.micro_customers.adapter.dto.EnderecoDto;
 import microservices.micro_customers.adapter.dto.TelefoneDto;
@@ -11,30 +12,43 @@ import java.util.Set;
 
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(name = "CustomerUpdateDtoResponse", description = "Transportador de dados para resposta ao usuário.")
 public record CustomerUpdateDtoResponse(
 
+        @Schema(name = "customerId", description = "Chave de identificação no banco de dados.", example = "22")
         Long customerId,
 
+        @Schema(name = "nomeCompleto", description = "Nome e sobrenome.", example = "Robert Martin")
         String nomeCompleto,
 
+        @Schema(name = "cpf", description = "Número do Cadastro de Pessoa Física.", example = "22076325042")
         String cpf,
 
+        @Schema(name = "dataNascimento", description = "Data oficial do nascimento.", example = "22/10/2024")
         String dataNascimento,
 
+        @Schema(name = "statusCadastro", description = "Fase do processo de cadastramento.", example = "INICIADO")
         StatusCadastroEnum statusCadastro,
 
+        @Schema(name = "email", description = "Endereço de Correio Eletrônico.", example = "teste@teste.com")
         String email,
 
+        @Schema(name = "telefones", description = "Coleção de números telefônicos.")
         Set<TelefoneDto> telefones,
 
+        @Schema(name = "endereços", description = "Coleção de endereços.")
         Set<EnderecoDto> enderecos,
 
+        @Schema(name = "createdAt", description = "Data e horário da operação de cadastrar.")
         LocalDateTime createdAt,
 
+        @Schema(name = "createdBy", description = "Identificador do titular da operação de cadastrar.")
         String createdBy,
 
+        @Schema(name = "updatedAt", description = "Data e horário da operação de atualizar.")
         LocalDateTime updatedAt,
 
+        @Schema(name = "updatedBy", description = "Identificador do titular da operação de atualizar.")
         String updatedBy
 
 ) { }
