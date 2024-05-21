22efddb58cf4ac90a6b7c30788b2a71b1c0ead47
juniorsmartins <juniorsoaresmartins@gmail.com>
2024-05-21 14:07:03 -0400

    FEAT: add RouteLocator e LoggingFilter em GatewayServer.

--- micro_gatewayserver/src/main/java/microsservices/micro_gatewayserver/MicroGatewayserverApplication.java
+++ micro_gatewayserver/src/main/java/microsservices/micro_gatewayserver/MicroGatewayserverApplication.java
@@ -14,25 +14,5 @@ public class MicroGatewayserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroGatewayserverApplication.class, args);
 	}
-
-	@Bean
-	public RouteLocator microservicesRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
-		return routeLocatorBuilder.routes()
-			.route(rota -> rota.path("/microservices/microcustomers/**")
-				.filters(filtro -> filtro.rewritePath("/microservices/microcustomers/(?<segment>.*)","/${segment}")
-					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-				.uri("lb://MICROCUSTOMERS")
-			)
-			.route(rota -> rota.path("/microservices/microempresas/**")
-				.filters(filtro -> filtro.rewritePath("/microservices/microempresas/(?<segment>.*)", "/${segment}")
-					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-				.uri("lb://MICROEMPRESAS")
-			)
-			.route(rota -> rota.path("/microservices/microemails/**")
-				.filters(filtro -> filtro.rewritePath("/microservices/microemails/(?<segment>.*)", "/${segment}")
-					.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-				.uri("lb://MICROEMAILS")
-			).build();
-	}
 }
 
