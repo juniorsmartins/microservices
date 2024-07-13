db2356e539fa9249e7d0932f59bb289d63826e0f
juniorsmartins <junior.soares.martins@proton.me>
2024-07-13 14:45:20 -0400

    CHORE: ajustes.

--- micro_gatewayserver/src/main/java/microsservices/micro_gatewayserver/configuration/ApiGatewayConfig.java
+++ micro_gatewayserver/src/main/java/microsservices/micro_gatewayserver/configuration/ApiGatewayConfig.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 import static org.springframework.cloud.gateway.support.RouteMetadataUtils.CONNECT_TIMEOUT_ATTR;
@@ -20,16 +22,22 @@ public class ApiGatewayConfig {
                 .path("/microcustomers/**")
                 .filters(filtro -> filtro.rewritePath("/microcustomers/(?<segment>.*)","/${segment}")
                     .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                    .retry(retryConfig -> retryConfig.setRetries(2) // Define o número máximo de tentativas automáticas de requisições em caso de falhas
+                        .setMethods(HttpMethod.GET, HttpMethod.PUT, HttpMethod.PATCH) // Diz que o retry será aplicado somente aos métodos especificados
+                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)) // Define a estratégia de backoff exponencial. O Gateway aumentará o tempo de espera entre tentativas (de 100ms para 1000ms) até a tentativa final, dobrando o tempo a cada tentativa. O parâmetro true indica que o tempo máximo de backoff será multiplicado pelo fator de multiplicação em cada tentativa.
                     .circuitBreaker(config -> config.setName("microcustomersCircuitBreaker") // Pode usar qualquer nome para o Circuit Breaker
                         .setFallbackUri("forward:/contactSupport"))) // Será acionado o fallback sempre que ocorrer erro
                         .metadata(CONNECT_TIMEOUT_ATTR, 3000) // Tempo máximo que o Gateway espera para estabelecer uma conexão com o serviço de destino
-                        .metadata(RESPONSE_TIMEOUT_ATTR, 8000) // Tempo máximo que o Gateway espera para receber uma resposta do serviço de destino após a conexão ser estabelecida
+                        .metadata(RESPONSE_TIMEOUT_ATTR, 12000) // Tempo máximo que o Gateway espera para receber uma resposta do serviço de destino após a conexão ser estabelecida
                 .uri("lb://MICROCUSTOMERS")
             )
             .route(rota ->
                 rota.path("/microempresas/**")
                 .filters(filtro -> filtro.rewritePath("/microempresas/(?<segment>.*)", "/${segment}")
                     .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                    .retry(retryConfig -> retryConfig.setRetries(2)
+                        .setMethods(HttpMethod.GET, HttpMethod.PUT, HttpMethod.PATCH)
+                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
                     .circuitBreaker(config -> config.setName("microempresasCircuitBreaker")
                         .setFallbackUri("forward:/contactSupport")))
                         .metadata(CONNECT_TIMEOUT_ATTR, 3000)
@@ -40,6 +48,9 @@ public class ApiGatewayConfig {
                 rota.path("/microemails/**")
                 .filters(filtro -> filtro.rewritePath("/microemails/(?<segment>.*)", "/${segment}")
                     .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                    .retry(retryConfig -> retryConfig.setRetries(2)
+                        .setMethods(HttpMethod.GET, HttpMethod.POST)
+                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
                     .circuitBreaker(config -> config.setName("microemailsCircuitBreaker")
                         .setFallbackUri("forward:/contactSupport")))
                         .metadata(CONNECT_TIMEOUT_ATTR, 3000)
--- micro_gatewayserver/src/main/java/microsservices/micro_gatewayserver/controller/FallbackController.java
+++ micro_gatewayserver/src/main/java/microsservices/micro_gatewayserver/controller/FallbackController.java
@@ -1,5 +1,7 @@
 package microsservices.micro_gatewayserver.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -7,9 +9,14 @@ import reactor.core.publisher.Mono;
 @RestController
 public class FallbackController {
 
+    private static final Logger log = LoggerFactory.getLogger(FallbackController.class);
+
     @RequestMapping("/contactSupport")
     public Mono<String> contactSupport() {
-        return Mono.just("Aconteceu um erro! Por favor, tente novamente em alguns minutos ou entre em contato com o suporte.");
+
+        log.error("Tentativa (retry) acionada!");
+
+        return Mono.just("Aconteceu um erro! Por favor, tente novamente ou entre em contato com o suporte.");
     }
 }
 
