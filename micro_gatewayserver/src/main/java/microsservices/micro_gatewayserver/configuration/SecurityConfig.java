3de654e7a018511b404c0568c7eeda5f55b3a05d
juniorsmartins <junior.soares.martins@proton.me>
2024-09-10 09:14:50 -0400

    FEAT: add autorization

--- micro_gatewayserver/src/main/java/microsservices/micro_gatewayserver/configuration/SecurityConfig.java
+++ micro_gatewayserver/src/main/java/microsservices/micro_gatewayserver/configuration/SecurityConfig.java
@@ -2,11 +2,17 @@ package microsservices.micro_gatewayserver.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -18,15 +24,22 @@ public class SecurityConfig {
         // serverHttpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().permitAll()) // Nenhuma requisição precisa ser autenticada
         // serverHttpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated()) // Todas as requisições precisam ser autenticadas
         serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll() // Especifica o tipo de requisição que precisa e que não precisa de autenticação
-                .pathMatchers("/microcustomers/**").authenticated()
-                .pathMatchers("/microempresas/**").authenticated()
-                .pathMatchers("/microemails/**").authenticated())
+                .pathMatchers("/microcustomers/**").hasRole("MICROCUSTOMERS")
+                .pathMatchers("/microempresas/**").hasRole("MICROEMPRESAS")
+                .pathMatchers("/microemails/**").hasRole("MICROEMAILS"))
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                        .jwt(Customizer.withDefaults()));
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
 
         serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return serverHttpSecurity.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
 
